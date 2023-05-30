b24900324c448d6b1ba69ee6b2bf7495c228e698
CotyStrumia <constanzastrumia28@gmail.com>
2023-05-30 20:02:33 -0300

    Se modifico el js

--- funciones.js
+++ funciones.js
@@ -3,17 +3,16 @@
  * @method calculo
  * @param {number} valor - contiene el valor del imput que ingresa el usuario
  */
+function calculo() {
+    var cant = parseInt(document.getElementById("Cantidad").value)||0;
+    var pUnit=parseInt(document.getElementById(PrecUnit).value) || 0;
+    var iva = parseInt(document.getElementById("cantIva").value)||0;
+    var subt = cant*pUnit;
+    var cantIva = (subt*iva)/100;
+    var total=subt+cantIva;
 
-function calculo(id, valor) {
-    let pUnit, cant, valiva, iva, total;
-   if (valor.includes(",")) {
-        valor = valor.replace(",", ".");
-    }
-    pUnit=Number(document.productos.precUnit.value);
-    cant=Number(document.productos.Cantidad.value);
-    valiva=Number(document.productos.porDeIva.value);
-    iva=Number(document.productos.IVA.value);
-    total=Number(document.productos.Total.value);
-    iva=((pUnit*cant)*100)/valiva;
-    total=(pUnit*cant)+iva;
+    document.getElementById("IVA").value= cantIva;
+    document.getElementById("Total").value=total;
 }
+
+

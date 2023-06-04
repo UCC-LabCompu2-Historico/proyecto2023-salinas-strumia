cd1f4d81369237145c13d94ed5af04cfdfb32e38
CotyStrumia <constanzastrumia28@gmail.com>
2023-06-03 23:03:26 -0300

    Se corrigio el codigo para generar filas, queda el canvas y agregar verificadores a los input para el usuario y el cliente

--- funciones.js
+++ funciones.js
@@ -4,42 +4,43 @@
  * @param {number} valor - contiene el valor del imput que ingresa el usuario
  */
 function calculoA(fila) {
-    const cant = parseInt(document.getElementById("Cantidad_" +fila).value)||0;
-    const pUnit=parseInt(document.getElementById("PrecUnit_" +fila).value) || 0;
-    const iva = parseInt(document.getElementById("cantIva").value)||0;
+    const cant = parseFloat(fila.querySelector(' .cantidad').value)||0;
+    const pUnit=parseFloat(fila.querySelector(' .precio').value) || 0;
+    const iva = parseFloat(fila.querySelector('.iva').value)||0;
     if (isNaN(pUnit)|| isNaN(cant)||pUnit<0||cant<0){
         alert('Los valores ingresados no pueden ser valores negativos ni letras');
-        document.getElementById("PrecUnit_" +fila).value=' ';
-        document.getElementById("Cantidad_" +fila).value=' ';
-        document.getElementById("IVA_" +fila).value=' ';
-        document.getElementById("Total_" +fila).value=' ';
+        fila.querySelector(' .cantidad').value=' ';
+        fila.querySelector(' .precio').value=' ';
+        fila.querySelector('.iva').value=' ';
+        fila.querySelector(' .IVA').value=' ';
         return;
     }
     const subt = cant*pUnit;
     const cantIva = (subt*iva)/100;
     const total=subt+cantIva;
 
-    document.getElementById("IVA_" +fila).value= cantIva;
-    document.getElementById("Total_" +fila).value=total;
+    fila.querySelector(' .ivaCalculado input').value= cantIva.toFixed(2);
+    fila.querySelector(' .total input').value=total.toFixed(2);
 }
 
-function calculoB(){
-    const cant=parseInt(document.getElementById("cant").value)||0;
-    const pUnit=parseInt(document.getElementById("pUnit").value)||0;
-    if (isNaN(pUnit)||isNaN(cant)||pUnit<0||cant<0){
-        alert('Los valores ingresados no pueden ser negativos ni letras');
-        document.getElementById("cant").value=' ';
-        document.getElementById("pUnit").value=' ';
-        document.getElementById("total").value=' ';
+function calculoB(fila){
+    const cant = parseFloat(fila.querySelector(' .cantidad').value)||0;
+    const pUnit=parseFloat(fila.querySelector(' .precio').value) || 0;
+
+    if (isNaN(pUnit)|| isNaN(cant)||pUnit<0||cant<0){
+        alert('Los valores ingresados no pueden ser valores negativos ni letras');
+        fila.querySelector(' .cantidad').value=' ';
+        fila.querySelector(' .precio').value=' ';
         return;
     }
+
     const total=cant*pUnit;
 
-    document.getElementById("total").value=total;
+    fila.querySelector(' .totalB input').value=total.toFixed(2);
 }
 
 function generarTablaA(){
-    let cantFilas=document.getElementById('cant_de_itemns').value;
+    var cantFilas=document.getElementById('cant_de_itemns').value;
     if (isNaN(cantFilas)||cantFilas<=0){
         alert('Por favor ingrese un valor numerico positivo');
         return;
@@ -48,80 +49,74 @@ function generarTablaA(){
     window.location.href='Factura_A.html?filas=' +cantFilas;
 }
 
+
+
 function cargarPaginaA(){
     let url= new URLSearchParams(window.location.search);
-    let cantFilas=urlParams.get('filas');
-
-    let tabla = document.createElement('table');
-    tabla.id='tabla';
+    let cantFilas = url.get('filas');
 
 
-    var tbody=document.createElement('cuerpoTabla');
-    for (let i=1; i<=cantFilas; i++){
-        let tr =document.createElement('tr');
+    let tbody=document.createElement('cuerpoTabla');
+    for (let i=0; i<cantFilas; i++){
+        let fila = cuerpoTabla.insertRow(i);
 
-        let producto = document.createElement('td');
-        producto.textContent='Producto_' +i;
-        tr.appendChild(producto);
+        let descripcionCell=fila.insertCell(0);
+        descripcionCell.innerHTML= '<input type="text" name="Descripcion" placeholder="Descripcion" class="descripcion" id="Descripcion">'
 
-        let PrecUnit=document.createElement('td');
-        let precioInput=document.createElement('input');
-        precioInput.type='number';
-        precioInput.id='PrecUnit_' +i;
-        precioInput.oninput=function (){
-            calculoA(i);
-        };
-        PrecUnit.appendChild(precioInput);
-        tr.appendChild(PrecUnit);
+        let cantidadCell=fila.insertCell(1);
+        cantidadCell.innerHTML=' <input type="number" name="Cantidad" placeholder="Cant" class="cantidad" id="Cantidad_" onchange="calculoA(this.parentNode.parentNode)" >'
 
-        let Cantidad=document.createElement('td');
-        let cantidadInput=document.createElement('input');
-        cantidadInput.type="number";
-        cantidadInput.id="Cantidad_" +i;
-        cantidadInput.oninput=function (){
-            calculoA(i);
-        };
-        Cantidad.appendChild(cantidadInput);
-        tr.appendChild(Cantidad);
+        let precioUnitarioCell = fila.insertCell(2);
+        precioUnitarioCell.innerHTML='<input type="number" name="PrecUnit" placeholder="Precio unit." class="precio" id="PrecUnit_1" onchange="calculoA(this.parentNode.parentNode)">'
 
-        let iva=document.createElement('td');
-        let ivaSelect=document.createElement('select');
-        ivaSelect.id='cantIva';
-        ivaSelect.onchange=function (){
-            calculoA(i);
-        };
-        let IVA=['0%','10.5%','21%','27%'];
-        for (let j=0; j<IVA.length; j++){
-            let opcion=document.createElement('opcion');
-            opcion.value=parseFloat(IVA[j]);
-            opcion.textContent=IVA[j];
-            ivaSelect.appendChild(opcion);
-        }
-        iva.appendChild(ivaSelect);
-        tr.appendChild(iva);
+        let ivaCell = fila.insertCell(3);
+        ivaCell.innerHTML='<select name="porDeIva" class="iva" onchange="calculoA(this.parentNode.parentNode)" id="cantIva">\n' +
+            '                <option value="0">0%</option>\n' +
+            '                <option value="10.5">10.5%</option>\n' +
+            '                <option value="21">21%</option>\n' +
+            '                <option value="27">27%</option>\n' +
+            '              </select>'
 
-        let totalIva=document.createElement('td');
-        let tIvaInput =document.createElement('input');
-        tIvaInput.type='text';
-        tIvaInput.id='IVA_' +i;
-        tIvaInput.readOnly=true;
-        totalIva.appendChild(tIvaInput);
-        tr.appendChild(totalIva);
+        let ivaCaluladoCell=fila.insertCell(4);
+        ivaCaluladoCell.innerHTML='<input type="text" name="IVA" placeholder="IVA" class="iva" id="IVA_1" readonly>'
+        ivaCaluladoCell.classList.add('ivaCalculado');
 
-        let total=document.createElement('td');
-        let totalInput=document.createElement('input');
-        totalInput.type='text';
-        totalInput.id='Total_'+i;
-        totalInput.readOnly=true;
-        total.appendChild(totalInput);
-        tr.appendChild(total);
-
-        tbody.appendChild(tr);
+        let totalCell=fila.insertCell(5);
+        totalCell.innerHTML='<input type="text" name="Total" placeholder="Total" id="Total_1" class="total" readonly>';
+        totalCell.classList.add('total');
     }
-    tabla.appendChild(tbody);
-    document.body.appendChild(tabla);
 }
+function generarTablaB(){
+    var cantFilas=document.getElementById('cant_de_itemns').value;
+    if (isNaN(cantFilas)||cantFilas<=0){
+        alert('Por favor ingrese un valor numerico positivo');
+        return;
+    }
+    window.location.href='FacturaB_C.html?filas=' +cantFilas;
+}
+function cargarPaginaB(){
+    let url= new URLSearchParams(window.location.search);
+    let cantFilas = url.get('filas');
 
+    let tbody=document.createElement('cuerpoTablaB');
+    for (let i=0; i<cantFilas; i++){
+        let fila = cuerpoTablaB.insertRow(i);
+
+        let descripcionCell=fila.insertCell(0);
+        descripcionCell.innerHTML= '<input type="text" name="Descripcion" placeholder="Descripcion" class="descripcion" id="Descripcion">'
+
+        let cantidadCell=fila.insertCell(1);
+        cantidadCell.innerHTML=' <input type="number" name="Cantidad" placeholder="Cant" class="cantidad" id="cant" onchange="calculoB(this.parentNode.parentNode)">'
+
+        let precioUnitarioCell = fila.insertCell(2);
+        precioUnitarioCell.innerHTML='<input type="number" name="PrecUnit" placeholder="Precio unit." class="precio" id="pUnit" onchange="calculoB(this.parentNode.parentNode)">'
+
+
+        let totalCell=fila.insertCell(3);
+        totalCell.innerHTML=' <input type="text" name="Total" placeholder="Total" class="total" id="total" readonly>';
+        totalCell.classList.add('totalB');
+    }
+}
 
 window.addEventListener('DOMContentLoaded', (event) => {
     // Obtener los datos ingresados en el formulario
